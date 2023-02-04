a7ca6d5d1f703524829792ce45a1a838bf91dfaa
Bang Soyoung <bjo8244@naver.com>
2023-02-04 15:11:07 +0900

    01. 연습문제

---
+++ src/ch01/PracticeEx01.java
@@ -0,0 +1,14 @@
+package ch01;
+// 붕어빵을 표현하기 위한 커스텀 자료형(heap)을 만드시오.
+// 클래스명 : 붕어빵 / 필드 : 붕어빵의 가격(1000), 맛(달콤함), 색깔(노란색)
+
+class Bread{
+    int price = 1000;
+    String taste = "sweet";
+    String color = "yellow";
+}
+public class PracticeEx01 {
+    public static void main(String[] args) {
+
+    }
+}
